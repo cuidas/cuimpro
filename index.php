8d6c0405c2eec41fb1be6ad57a56d3cebdea9ef5
cuidas <cuidas@gmx.net>
2014-04-06 12:19:05 +0000

    simplem image output

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?
-echo 'foo';
+/* Attempt to open */
+$imgname = ('http://commons.wikimedia.org/wiki/File:Facebook_like_thumb.png');
+$im = @imagecreatefrompng($imgname);
+
+/* See if it failed */
+if(!$im)
+{
+    /* Create a blank image */
+    $im  = imagecreatetruecolor(150, 30);
+    $bgc = imagecolorallocate($im, 255, 255, 255);
+    $tc  = imagecolorallocate($im, 0, 0, 0);
+
+    imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+
+    /* Output an error message */
+    imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+}
+
+
+header('Content-Type: image/png');
+
+imagepng($im);
+imagedestroy($im);
 ?>
